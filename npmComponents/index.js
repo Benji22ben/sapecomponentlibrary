551127be8fff4ea7528d75c980654cf1da71ea98
Benji22ben <benjamin.marques-balula@etu.univ-smb.fr>
2023-11-15 16:14:43 +0100

    fix: forgot this part about npm

---
+++ npmComponents/index.js
@@ -0,0 +1,5 @@
+import Button from "./src/components/Button";
+import TouchableOpacity from "./src/components/TouchableOpacity";
+import TouchableHighlight from "./src/components/TouchableHighlight";
+
+export { Button, TouchableOpacity, TouchableHighlight };
